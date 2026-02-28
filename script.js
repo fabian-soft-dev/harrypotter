06ebc6bd0d31071b520931ecc4f8685b0c94eb34
fabian-soft-dev <fabiusc1816@gmail.com>
2026-02-28 08:23:13 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,26 +1,40 @@
 const container = document.getElementById('list-container');
+const botones = document.querySelectorAll('#filtros button');
+
+let allCharacters = [];
+
+//carga de personajes
 
 async function getCharacters() {
-    console.log("--- PASO 1: Solicitando datos... ---");
-    
+    console.log("Solicitando datos...");
+
     try {
         const response = await fetch('https://hp-api.onrender.com/api/characters');
         const data = await response.json();
-        
-        const limitedData = data.filter(c => c.image !== "").slice(0, 30);
-        
-        console.log("--- PASO 2: Array de objetos recibido ---");
-        console.table(limitedData);
 
-        limitedData.forEach((personaje, index) => {
-            console.log(`Renderizando fila #${index + 1}:`, personaje.name);
-            createRow(personaje);
-        });
+        // guardo solo los que tienen imagen y limito a 25
+        allCharacters = data
+            .filter(c => c.image !== "")
+            .slice(0, 25);
+
+        renderCharacters(allCharacters);
 
     } catch (error) {
         console.error("Error cargando la API:", error);
     }
 }
+
+//renderizar lista
+
+function renderCharacters(characters) {
+    container.innerHTML = ""; // Limpiar lista
+
+    characters.forEach(char => {
+        createRow(char);
+    });
+}
+
+//creación de cada tarjeta
 
 function createRow(char) {
     const row = document.createElement('div');
@@ -35,17 +49,33 @@ function createRow(char) {
         </div>
     `;
 
-    row.addEventListener("click", () =>{
-        window.location.href = `info.html?name=${encodeURIComponent(char.name)}`;
-        row.classList.toggle("active");
-
+    row.addEventListener('click', () => {
+        window.location.href = `detalles.html?name=${encodeURIComponent(char.name)}`;
     });
-
 
     container.appendChild(row);
 }
 
+//filtros de casa
 
+
+botones.forEach(boton => {
+    boton.addEventListener('click', () => {
+        const casa = boton.dataset.house;
+
+        if (casa === "all") {
+            renderCharacters(allCharacters);
+        }
+        else if (casa === "none") {
+            const sinCasa = allCharacters.filter(c => !c.house);
+            renderCharacters(sinCasa);
+        }
+        else {
+            const filtrados = allCharacters.filter(c => c.house === casa);
+            renderCharacters(filtrados);
+        }
+    });
+});
 
 
 getCharacters();
